c70ba3d931b7eced9084a69e0e314ee050c4b265
James Seward <james.seward@oath.com>
2017-09-21 15:55:52 -0700

    Tweak VPN colours

--- scripts/display_vpn.sh
+++ scripts/display_vpn.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if netstat -nr -f inet | grep gpd0 > /dev/null; then
-	echo "#[bg=colour70,fg=colour254] VPN "
+	echo "#[bg=colour70,fg=colour255] VPN "
 else
-	echo "#[bg=colour214,fg=colour254] VPN "
+	echo "#[bg=colour214,fg=colour240] VPN "
 fi
 
