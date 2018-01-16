42586b452a06576cbcbb5a64c49269070adccc62
James Seward <james.seward@oath.com>
2018-01-16 11:24:38 +0000

    Handle being on wifi and Y! VPN too

--- scripts/display_vpn.sh
+++ scripts/display_vpn.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
-if netstat -nr -f inet | grep gpd0 > /dev/null; then
-	echo "#[bg=colour70,fg=colour255] VPN "
-else
-	echo "#[bg=colour214,fg=colour240] VPN "
-fi
+# Check if we're on the corp wifi
+network_name=$( /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport --getinfo | grep -E '\bSSID:' | cut -d: -f2- | sed -E -e 's/^ (.+)/\1/' )
 
+if [[ $network_name == "extended-stay" ]]; then
+	echo "#[bg=color0,fg=color75] WiFi "
+else
+	if netstat -nr -f inet | grep gpd0 > /dev/null; then
+		echo "#[bg=colour70,fg=colour255] VPN "
+	elif netstat -nr -f inet | grep tun4 > /dev/null; then
+		echo "#[bg=colour53,fg=colour255] VPN "
+	else
+		echo "#[bg=colour214,fg=colour240] VPN "
+	fi
+fi
