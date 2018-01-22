3a847663d83614237b76b8e2da384d64f9c9b4ec
James Seward <james.seward@oath.com>
2018-01-22 09:24:37 -0500

    Update logic for VPN/wifi

--- scripts/display_vpn.sh
+++ scripts/display_vpn.sh
@@ -3,14 +3,12 @@
 # Check if we're on the corp wifi
 network_name=$( /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport --getinfo | grep -E '\bSSID:' | cut -d: -f2- | sed -E -e 's/^ (.+)/\1/' )
 
-if [[ $network_name == "extended-stay" ]]; then
-	echo "#[bg=color0,fg=color75] WiFi "
+if netstat -nr -f inet | grep gpd0 > /dev/null; then
+	echo "#[bg=colour70,fg=colour255] VPN "
+elif ifconfig | grep -A2 -F utun | grep -E 'inet (172\.141\.|10\.89\.)' > /dev/null; then
+	echo "#[bg=colour53,fg=colour255] VPN "
+elif [[ $network_name == "extended-stay" ]]; then
+	echo "#[bg=colour0,fg=colour75] WiFi "
 else
-	if netstat -nr -f inet | grep gpd0 > /dev/null; then
-		echo "#[bg=colour70,fg=colour255] VPN "
-	elif netstat -nr -f inet | grep tun4 > /dev/null; then
-		echo "#[bg=colour53,fg=colour255] VPN "
-	else
-		echo "#[bg=colour214,fg=colour240] VPN "
-	fi
+	echo "#[bg=colour214,fg=colour240] VPN "
 fi
