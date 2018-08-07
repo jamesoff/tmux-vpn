c804433b0787244240d9d454cd0cd4a36926d0c5
James Seward <sewardjm@amazon.com>
2018-08-07 16:35:50 +0100

    Update office wifi

--- scripts/display_vpn.sh
+++ scripts/display_vpn.sh
@@ -5,7 +5,7 @@ network_name=$( /System/Library/PrivateFrameworks/Apple80211.framework/Versions/
 
 if netstat -nr -f inet | grep utun1 > /dev/null; then
 	echo "#[bg=colour70,fg=colour255] VPN "
-elif [[ $network_name == "extended-stay" ]]; then
+elif [[ $network_name == "wpa2" ]]; then
 	echo "#[bg=colour0,fg=colour75] WiFi "
 else
 	echo "#[bg=colour214,fg=colour240] VPN "
