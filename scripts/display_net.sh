1dfaa1ad74f02bf5216c4a112d4c6f845e33932e
James Seward <sewardjm@amazon.com>
2019-01-17 18:50:04 +0000

    Update phone network name

--- scripts/display_net.sh
+++ scripts/display_net.sh
@@ -11,8 +11,8 @@ age=$(( $( date +%s ) - last_check ))
 
 if [[ $age -gt 30 ]]; then
 	network_name=$( /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport --getinfo | grep -E '\bSSID:' | cut -d: -f2- | sed -E -e 's/^ (.+)/\1/' )
-	if ping -c 1 -t 2 www.amazon.com > /dev/null 2> /dev/null; then
-		if [[ $network_name == "iPhone" || $network_name == "iPhoan X" ]]; then
+	if http https://www.google.co.uk > /dev/null 2> /dev/null; then
+		if [[ $network_name == "iPhoan X Smax" || $network_name == "iPhoan X" ]]; then
 			echo "#[bg=colour70,fg=colour255] MOB " > connectivity-check
 		else
 			echo "" > connectivity-check
