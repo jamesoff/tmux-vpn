72274f9bc01b7df96158f9ac61fc4cf47e7b3240
James Seward <sewardjm@amazon.com>
2019-04-10 18:33:22 +0100

    Use hidden file for connectivity check

--- scripts/display_net.sh
+++ scripts/display_net.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+connectivity_file="$HOME/.connectivity-check"
+
 # check for network connectivity
 last_check=0
 
-if [[ -r connectivity-check ]]; then
-	last_check=$( stat -f %m connectivity-check 2> /dev/null )
+if [[ -r $connectivity_file ]]; then
+	last_check=$( stat -f %m "$connectivity_file" 2> /dev/null )
 fi
 
 age=$(( $( date +%s ) - last_check ))
@@ -13,13 +15,13 @@ if [[ $age -gt 30 ]]; then
 	network_name=$( /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport --getinfo | grep -E '\bSSID:' | cut -d: -f2- | sed -E -e 's/^ (.+)/\1/' )
 	if http https://www.google.co.uk > /dev/null 2> /dev/null; then
 		if [[ $network_name == "iPhoan X Smax" || $network_name == "iPhoan X" ]]; then
-			echo "#[bg=colour70,fg=colour255] ﰒ  " > connectivity-check
+			echo "#[bg=colour70,fg=colour255] ﰒ  " > "$connectivity_file"
 		else
-			echo "" > connectivity-check
+			echo "" > "$connectivity_file"
 		fi
 	else
-		echo "#[bg=colour1,fg=colour255]   " > connectivity-check
+		echo "#[bg=colour1,fg=colour255]   " > "$connectivity_file"
 	fi
 fi
 
-cat connectivity-check
+cat "$connectivity_file"
