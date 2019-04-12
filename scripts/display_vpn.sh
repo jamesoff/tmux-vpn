aad96caa98e81f6298998351bf5f9dd5370ed001
James Seward <sewardjm@amazon.com>
2019-04-12 13:39:37 +0100

    Change utun grep to be less picky

--- scripts/display_vpn.sh
+++ scripts/display_vpn.sh
@@ -3,7 +3,7 @@
 # Check if we're on the corp wifi
 network_name=$( /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport --getinfo | grep -E '\bSSID:' | cut -d: -f2- | sed -E -e 's/^ (.+)/\1/' )
 
-if netstat -nr -f inet | grep utun1 > /dev/null; then
+if netstat -nr -f inet | grep utun > /dev/null; then
 	echo "#[bg=colour70,fg=colour255]   "
 elif [[ $network_name == "wpa2" ]]; then
 	echo "#[bg=colour75,fg=colour0]   "
